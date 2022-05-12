8595f131c8b81345f0604321ddeff479368370e0
Xavi Yamuza <xavi_ym@hotmail.com>
2022-05-12 18:07:15 +0200

    diseño 2 pdf

--- controllers/Entrada.class.php
+++ controllers/Entrada.class.php
@@ -14,17 +14,34 @@ class Entrada extends Controller
         require_once __DIR__ . "../../model/bootstrap.php";
 
         $entrada = $entityManager->getRepository("Thos\Entrada")->find($ref);
+        
 
         /* Valores para la Entrada */
         $tituloEvent = $entrada->getEvent()->getTitol();
         $subtitolEvent = $entrada->getEvent()->getSubtitol();
+        
+        $pattern = "/img/";
         $imatgeEvent = $entrada->getEvent()->getImatge();
+    
+
+            if(preg_match($pattern, $imatgeEvent)){
+                $imgEvento = $imatgeEvent;
+            }else{
+                $imgEvento = 'img/'. $imatgeEvent;
+            }
+       
+        
+        
         $fila = $entrada->getFila();
         $butaca = $entrada->getButaca();
         $dni = $entrada->getCompardor();
         $fechaEvento = $entrada->getData()->getData();
         $horaEvento = $entrada->getData()->getHora();
         $lugarEvento = $entrada->getLocalitzacio()->getLloc();
+        $direcionEvento = $entrada->getLocalitzacio()->getAdreca();
+        $localitatEvento = $entrada->getLocalitzacio()->getLocalitat();
+        $zonaEvento = $entrada->getZona()->getDescripcio();
+        $confirmacion = $entrada->getPagament()->getReferenciaExterna();
 
         $data = 'http://localhost/M7_Entrada/?ref='.$ref.'';
 
@@ -45,15 +62,20 @@ class Entrada extends Controller
              }
              th, td {
                 width: 25%;
-                text-align: center;
+               
                 text-align: left;
                 vertical-align: top;
                 border: 1px solid #000;
                 border-collapse: collapse;
              }
-             .center{
+             th{
+                text-align: center;
+             }
+             .center-img{
                  text-align: center;
                  margin:0 auto;
+                 width: 100%
+                 height: 300px;
              }
              .alert{
                 border: 2px;
@@ -61,6 +83,14 @@ class Entrada extends Controller
                 width: 100%;
                 
              }
+             .logo{
+                 width: 150px;
+             }
+             .titulo{
+                 background-color: #961847;
+                 color: white;
+             }
+
              
             
         </style>
@@ -69,33 +99,51 @@ class Entrada extends Controller
         <table>
 
         <caption>
-                <h1>' . $tituloEvent . '</h1>
+        <img class="logo" src="img/logo.png">
+                <h1 class="titulo">' . $tituloEvent . '</h1>
                 <p>' . $subtitolEvent . '</p>
         </caption>
         <thead>
             <tr>
-                <th>
+                <th colspan="2">
                     <h2>Fila</h2>
                 </th>
-                <th>
+                <th colspan="2">
                     <h2>Butaca</h2>
                 </th>
                 <th colspan="2">
-                    <h2>Fecha</h2>
+                    <h2>Zona</h2>
                 </th>
-                <th colspan="2">
-                    <h2>Lugar</h2>
-                </th>
+                
 
             </tr>
         </thead>
 
         <tbody>
             <tr>
-                <td>' . $fila . '</td>
-                <td>' . $butaca . '</td>
+                <td colspan="2">' . $fila . '</td>
+                <td colspan="2">' . $butaca . '</td>
+                <td colspan="2">' . $zonaEvento . '</td>
+                
+           
+            </tr>
+            <tr>
+                    <th colspan="2">
+                    <h2>Fecha</h2>
+                </th>
+                <th colspan="2">
+                    <h2>Lugar</h2>
+                </th>
+                <th colspan="2">
+                    <h2>QR</h2>
+                </th>
+           
+            </tr>
+            <tr>
+
                 <td colspan="2">' . $fechaEvento . ' a las '. $horaEvento .'</td>
-                <td colspan="2">' . $lugarEvento . '</td>
+                <td colspan="2">' . $lugarEvento .' - '.$direcionEvento.'('.$localitatEvento.')</td>
+                <td colspan="2">' .$this->generateQR($data). '</td>
            
             </tr>
             
@@ -103,17 +151,17 @@ class Entrada extends Controller
         
         <tfoot>
             <tr>
-                <th scope="row" colspan="6">
-             <div class="center">
-                <img class="event-img" src="img/' . $imatgeEvent . '">
-                '.$this->generateQR($data).'
-                <p class="alert">Enseña este código QR en la entrada para acceder al evento</p>
+                <th scope="row" colspan="6" class="center-img">
+             <div>
+               
+                <img class="event-img" src="'.$imgEvento.'"/>
+                <p class="alert">Enseña el código QR en la entrada para acceder al evento</p>
                 
              </div>
                 </th>
             </tr>
         </tfoot>
-        '. $this->generateCodeBar('0X123172389X') . '
+        '. $this->generateCodeBar($confirmacion) . '
 
      </table>'
        
@@ -121,9 +169,17 @@ class Entrada extends Controller
 
         $mpdf = new \Mpdf\Mpdf();
         $mpdf->WriteHTML($htmlEntrada);
-        $mpdf->Output('Entrada'.$ref.'.pdf');
+        $mpdf->Output('Entrada.pdf');
         
         echo "<h1>Entrada generada correctamente</h1>";
+
+        $pattern = "/img/";
+             echo $imatgeEvent. '<br>';
+        
+        //echo preg_match($pattern, $imatgeEvent);
+
+        echo $imgEvento;
+             
 
         //echo $htmlEntrada;
     }
