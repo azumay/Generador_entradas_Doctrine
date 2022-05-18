59714483fd35e22f5ae4d6af02367496fac48c5d
Xavi Yamuza <xavi_ym@hotmail.com>
2022-05-18 18:13:03 +0200

    add-CodeBar

--- controllers/Entrada.class.php
+++ controllers/Entrada.class.php
@@ -43,6 +43,7 @@ class Entrada extends Controller
         $localitatEvento = $entrada->getLocalitzacio()->getLocalitat();
         $zonaEvento = $entrada->getZona()->getDescripcio();
         $confirmacion = $entrada->getPagament()->getReferenciaExterna();
+        $this->generateCodeBar($confirmacion);
 
         $data = 'http://localhost/M7_Entrada/?ref='.$ref.'';
 
@@ -53,7 +54,7 @@ class Entrada extends Controller
                 color: red; 
             }
             .event-img {
-                width: 50%;
+                width:50%;
             }
             table {
                 width: 100%;
@@ -63,14 +64,17 @@ class Entrada extends Controller
              }
              th, td {
                 width: 25%;
-               
-                text-align: left;
+                font-family: Arial;
+                text-align: center;
                 vertical-align: top;
+                font-size: 20px;
                 border: 1px solid #000;
                 border-collapse: collapse;
              }
              th{
                 text-align: center;
+                background-color: #961847;
+                color: white;
              }
              .center-img{
                  text-align: center;
@@ -90,17 +94,22 @@ class Entrada extends Controller
              .titulo{
                  background-color: #961847;
                  color: white;
+                 font-family: Arial;
              }
-
+             .qr-code{
+                 width: 150px;
+             }
+             
              
             
         </style>
 
-        
+        <img class="logo" src="img/atrapalo-logo.png">
         <table>
 
         <caption>
-        <img class="logo" src="img/logo.png">
+        
+        
                 <h1 class="titulo">' . $tituloEvent . '</h1>
                 <p>' . $subtitolEvent . '</p>
         </caption>
@@ -142,7 +151,7 @@ class Entrada extends Controller
             </tr>
             <tr>
 
-                <td colspan="2">' . $fechaEvento . ' a las '. $horaEvento .'</td>
+                <td colspan="2">' . $fechaEvento . '<br> a las '. $horaEvento .'</td>
                 <td colspan="2">' . $lugarEvento .' - '.$direcionEvento.'('.$localitatEvento.')</td>
                 <td colspan="2">' .$this->generateQR($data). '</td>
            
@@ -152,17 +161,19 @@ class Entrada extends Controller
         
         <tfoot>
             <tr>
-                <th scope="row" colspan="6" class="center-img">
+                <td scope="row" colspan="6" class="center-img">
              <div>
                
                 <img class="event-img" src="'.$imgEvento.'"/>
                 <p class="alert">Enseña el código QR en la entrada para acceder al evento</p>
-                
              </div>
-                </th>
+                </td>
             </tr>
+            <br>
+            <img src="img/CodeBar.png"/>
+            
         </tfoot>
-        '. $this->generateCodeBar($confirmacion) . '
+      
 
      </table>'
        
@@ -181,12 +192,14 @@ class Entrada extends Controller
     {
 
         $qr = new QRCode;
-        return '<img src="' . $qr->render($url) . '" alt="QR Code" />';
+        return '<img class="qr-code" src="' . $qr->render($url) . '" alt="QR Code" />';
     }
 
     public function generateCodeBar($num)
     {
-        $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-        return $generator->getBarcode($num, $generator::TYPE_CODE_128);
+        $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        
+        file_put_contents('img/CodeBar.png', $generator->getBarcode($num, $generator::TYPE_CODE_128));
+        
     }
 }
